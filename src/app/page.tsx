9138cc910ce343e0fb2659a5349f97d112313fbd
denandras <d.enandras@gmail.com>
2026-06-21 15:29:36 +0200

    style: revert to subtle 5% white balance cooling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ function HeroContent() {
         {/* Image first on mobile and desktop */}
         <div className="hero-image">
           <img 
-            src="/portrait.jpg" 
+            src="/portrait.jpg?v=2" 
             alt="Elek Petra" 
             width={1600}
             height={2133}
